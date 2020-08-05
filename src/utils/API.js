f3bbc27b2082e07d33557f6d2f95832989702f05
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-08-05 17:29:54 -0400

    updates to API.js, WIP

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,33 @@
 import axios from "axios";
 
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+const GIPHYURL = "https://api.giphy.com/v1/gifs/search?q=";
+const GIPHYAPIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+
+const MWURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
+const MWAPIKEY = "?key=edeb60ac-21d3-4109-93d4-16ae042e92ec";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
-export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
-  }
+const API = {
+  search: Promise.all([getGiphy(), getDictionary()])
+  .then(function (results) {
+    const acct = results[0];
+    const perm = results[1];
+  })
 };
+
+function getGiphy(query) {
+  return axios.get(GIPHYURL + query + GIPHYAPIKEY);
+}
+
+function getDictionary(query) {
+  console.log("dictionary", query)
+return axios.get(MWURL + query + MWAPIKEY);
+//.then(function (response) {
+  // handle success
+  //console.log(response);
+  //return response
+}
+
+export default API;
+
+
