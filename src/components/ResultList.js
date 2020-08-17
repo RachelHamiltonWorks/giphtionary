86d8cdf2b0a7e9b478ce6db7ad91330e2f40f93f
Coop <61427314+codelucid@users.noreply.github.com>
2020-08-17 18:10:16 -0400

    creation of DisplayWord component and rendering the word below the search box and button but above the definition display

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "reactstrap";
+import DisplayWord from "./DisplayWord.js";
 
 function ResultList(props) {
   // console.log(props.resultsDictionary);
   return (
     <Container>
+      <DisplayWord word={props.resultsDictionary} />
       {props.resultsDictionary.shortdef && <h2>{props.resultsDictionary.shortdef.map((def) => 
       <p>{def}</p>)}</h2>}
       <div id="wrapper">
