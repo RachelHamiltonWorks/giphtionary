bfb6b947f6767cef3d663118edaf025ad4700e47
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-08-22 17:39:48 -0400

    please, please god

--- public/favicongif.js
+++
@@ -1,9 +0,0 @@
-//working on a favicon gif - not currently operable, and only to do with extra time
-
-
-$.tbFavAnime({
-	images: ['favicon-32x32.png', 'favicon2.png'],
-	imgPath: 'src/assets',
-	type: 0,
-	interval: 300,
- });
--- src/components/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Logo = () => (
-<h1>giphtionary</h1>
-);
-
-export default Logo;
