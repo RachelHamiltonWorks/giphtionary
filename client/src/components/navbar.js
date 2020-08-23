069ceb2db07a75a9ff322b30d28e4a4469d4afa2
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-08-23 14:35:49 -0400

    Update navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -57,58 +57,101 @@ const NavBar = () => {
           </Nav>
           <Nav className="d-none d-md-block" navbar>
             {!isAuthenticated && (
-              <Nav className="d-md-none" navbar>
-                <NavItem>
-                  <Button
-                    id="qsLoginBtn"
-                    color="info"
-                    block
-                    onClick={() => loginWithRedirect({})}
-                  >
-                    Log in
-                  </Button>
-                </NavItem>
-              </Nav>
+              <NavItem>
+                <Button
+                  id="qsLoginBtn"
+                  color="info"
+                  className="btn-margin"
+                  onClick={() => loginWithRedirect()}
+                >
+                  Log in
+                </Button>
+              </NavItem>
             )}
             {isAuthenticated && (
-              <Nav
-                className="d-md-none justify-content-between"
-                navbar
-                style={{ minHeight: 170 }}
-              >
-                <NavItem>
-                  <span className="user-info">
-                    <img
-                      src={user.picture}
-                      alt="Profile"
-                      className="nav-user-profile d-inline-block rounded-circle mr-3"
-                      width="50"
-                    />
-                    <h6 className="d-inline-block">{user.name}</h6>
-                  </span>
-                </NavItem>
-                <NavItem>
-                  <FontAwesomeIcon icon="user" className="mr-3" />
-                  <RouterNavLink
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret id="profileDropDown">
+                  <img
+                    src={user.picture}
+                    alt="Profile"
+                    className="nav-user-profile rounded-circle"
+                    width="50"
+                  />
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>{user.name}</DropdownItem>
+                  <DropdownItem
+                    tag={RouterNavLink}
                     to="/profile"
+                    className="dropdown-profile"
                     activeClassName="router-link-exact-active"
                   >
-                    Profile
-                  </RouterNavLink>
-                </NavItem>
-                <NavItem>
-                  <FontAwesomeIcon icon="power-off" className="mr-3" />
-                  <RouterNavLink
-                    to="#"
+                    <FontAwesomeIcon icon="user" className="mr-3" /> Profile
+                  </DropdownItem>
+                  <DropdownItem
                     id="qsLogoutBtn"
                     onClick={() => logoutWithRedirect()}
                   >
-                    Log out
-                  </RouterNavLink>
-                </NavItem>
-              </Nav>
+                    <FontAwesomeIcon icon="power-off" className="mr-3" /> Log
+                    out
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             )}
-          </Collapse>
+          </Nav>
+          {!isAuthenticated && (
+            <Nav className="d-md-none" navbar>
+              <NavItem>
+                <Button
+                  id="qsLoginBtn"
+                  color="info"
+                  block
+                  onClick={() => loginWithRedirect({})}
+                >
+                  Log in
+                </Button>
+              </NavItem>
+            </Nav>
+          )}
+          {isAuthenticated && (
+            <Nav
+              className="d-md-none justify-content-between"
+              navbar
+              style={{ minHeight: 170 }}
+            >
+              <NavItem>
+                <span className="user-info">
+                  <img
+                    src={user.picture}
+                    alt="Profile"
+                    className="nav-user-profile d-inline-block rounded-circle mr-3"
+                    width="50"
+                  />
+                  <h6 className="d-inline-block">{user.name}</h6>
+                </span>
+              </NavItem>
+              <NavItem>
+                <FontAwesomeIcon icon="user" className="mr-3" />
+                <RouterNavLink
+                  to="/profile"
+                  activeClassName="router-link-exact-active"
+                >
+                  Profile
+                </RouterNavLink>
+              </NavItem>
+              <NavItem>
+                <FontAwesomeIcon icon="power-off" className="mr-3" />
+                <RouterNavLink
+                  to="#"
+                  id="qsLogoutBtn"
+                  onClick={() => logoutWithRedirect()}
+                >
+                  Log out
+                </RouterNavLink>
+              </NavItem>
+            </Nav>
+          )}
+        </Collapse>
         {/* </Container> */}
       </Navbar>
     </div>
