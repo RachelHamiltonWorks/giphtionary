d00bbc09a76e772e6e7d1a89745f9cf620b1917b
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-08-23 15:45:44 -0400

    Update Content.js

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 
-<<<<<<< Updated upstream
-=======
-import { Row, Container } from 'reactstrap';
-
-import ResultList from './ResultList';
-
->>>>>>> Stashed changes
 class Content extends Component {
   render() {
     return (
